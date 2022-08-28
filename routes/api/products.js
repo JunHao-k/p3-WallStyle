63f45616286c66f76acad05c238ee18da9e641c9
JunHao-k <koh.junhao@trentglobal.edu.sg>
2022-08-28 09:34:19 +0000

    Updates make to the products api

--- routes/api/products.js
+++ routes/api/products.js
@@ -3,6 +3,12 @@ const router = express.Router()
 const { Product, Theme, Variant } = require('../../models')
 const productDataLayer = require('../../dal/products')
 
+router.get("/" , async (req , res) => {
+    const allProducts = await productDataLayer.getAllProducts()
+    res.status(200)
+    res.json(allProducts)
+})
+
 
 router.get('/:product_id' , async (req , res) => {
     const product = await productDataLayer.getProductById(req.params.product_id)
