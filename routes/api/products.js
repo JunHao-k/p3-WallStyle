b5689359ddc9bff246aa0aa85bf21ebdcf192d02
JunHao-k <koh.junhao@trentglobal.edu.sg>
2022-08-29 14:21:51 +0000

    Added new route to rpoducts

--- routes/api/products.js
+++ routes/api/products.js
@@ -28,6 +28,12 @@ router.get('/variants/:product_id' , async (req , res) => {
     res.json(variants)
 })
 
+router.get('/products/:product_id' , async (req , res) => {
+    const products = await productDataLayer.getProductById(req.params.product_id)
+    res.status(200)
+    res.json(products)
+})
+
 
 router.get('/:product_id' , async (req , res) => {
     const product = await productDataLayer.getProductById(req.params.product_id)
